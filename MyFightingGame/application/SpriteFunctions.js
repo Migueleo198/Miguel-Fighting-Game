8ce8f21dcb281b8a8e744e4f12fafb79932f43e7
Miguelz <62753251+Migueleo198@users.noreply.github.com>
2025-01-22 07:38:59 +0100

    reupload

---
+++ MyFightingGame/application/SpriteFunctions.js
@@ -0,0 +1,142 @@
+import { Sprite } from '../domain/Sprite.js';
+
+
+
+class SpriteFunctions {
+    constructor() {
+
+    }
+
+
+    loadPlayer(x, y) {
+
+        this.player = new Sprite({
+            position: {
+                x: x,
+                y: y
+            },
+            velocity: {
+                x: 0,
+                y: 0
+            },
+            imageSrc : '../assets/Martial Hero 2/Sprites/Idle.png',
+            scale: 2.1,
+            framesMax: 4,
+            offset:{
+                x:215,
+                y:120
+            },
+            sprites:{
+                idle:{
+                    imgSrc:'../assets/Martial Hero 2/Sprites/Idle.png',
+                    framesMax:4
+                },
+                run:{
+                    imgSrc:'../assets/Martial Hero 2/Sprites/Run.png',
+                    framesMax:8
+                },
+                jump:{
+                    imgSrc:'../assets/Martial Hero 2/Sprites/Jump.png',
+                    framesMax:2
+                },
+                attack1:{
+                    imgSrc:'../assets/Martial Hero 2/Sprites/Attack1.png',
+                    framesMax:4
+                },
+                 takeHit:{
+                    imgSrc:'../assets/Martial Hero 2/Sprites/Take hit.png',
+                    framesMax:3
+                },
+                death:{
+                    imgSrc:'../assets/Martial Hero 2/Sprites/Death.png',
+                    framesMax:7
+                },
+                fall:{
+                    imgSrc:'../assets/Martial Hero 2/Sprites/Fall.png',
+                    framesMax:2
+                },
+                attack2:{
+                    imgSrc:'../assets/Martial Hero 2/Sprites/Attack2.png',
+                    framesMax:4
+                }
+            },
+            type:'player'
+           
+        });
+
+        this.player.drawPlayer();
+
+    }
+
+    loadEnemy(x, y) {
+
+        this.enemy = new Sprite({
+            position: {
+                x: x,
+                y: y
+            },
+            velocity: {
+                x: 0,
+                y: 0
+            },
+            imageSrc : '../assets/Martial Hero 2/Sprites/Idle2.png',
+            scale:2.1,
+            framesMax:4,
+            offset:{
+                x:215,
+                y:120
+            },
+            sprites:{
+                idle:{
+                    imgSrc:'../assets/Martial Hero 2/Sprites/Idle2.png',
+                    framesMax:4
+                },
+                run:{
+                    imgSrc:'../assets/Martial Hero 2/Sprites/RunEnemy.png',
+                    framesMax:8
+                },
+                jump:{
+                    imgSrc:'../assets/Martial Hero 2/Sprites/JumpEnemy.png',
+                    framesMax:2
+                },
+                attack1:{
+                    imgSrc:'../assets/Martial Hero 2/Sprites/Attack1Enemy.png',
+                    framesMax:4
+                },
+                takeHit:{
+                    imgSrc:'../assets/Martial Hero 2/Sprites/Take hit Enemy.png',
+                    framesMax:3
+
+                },
+                death:{
+                    imgSrc:'../assets/Martial Hero 2/Sprites/DeathLeft.png',
+                    framesMax:7
+                },
+                fall:{
+                    imgSrc:'../assets/Martial Hero 2/Sprites/FallLeft.png',
+                    framesMax:2
+                },
+                attack2:{
+                    imgSrc:'../assets/Martial Hero 2/Sprites/Attack2Left.png',
+                    framesMax:4
+                }
+            },
+            type:'enemy'
+        });
+
+        this.enemy.drawEnemy();
+
+    }
+
+    
+
+
+}
+
+
+
+
+
+
+
+export { SpriteFunctions };
