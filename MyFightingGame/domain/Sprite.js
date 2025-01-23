a74a878bcc5064a532ff3cb10201f66444377c10
Miguelz <62753251+Migueleo198@users.noreply.github.com>
2025-01-23 14:07:47 +0100

    reupload

---
+++ MyFightingGame/domain/Sprite.js
@@ -0,0 +1,356 @@
+const canvas = document.querySelector('canvas');
+const c = canvas.getContext('2d');
+
+
+class Sprite {
+
+
+    constructor({ position, velocity, imageSrc, scale = 1, framesMax, offset, sprites,type }) {
+        this.position = position;
+        this.velocity = velocity;
+        this.width = 70;
+        this.height = 150;
+        this.jumpCounter = 0;
+        this.positionHitBox = {
+            position:{
+                x:this.position.x+100,
+                y:this.position.y
+            }
+
+        }
+        this.hitBox = {
+            position: this.positionHitBox,
+            width: 300,
+            height: 50
+        }
+        
+        this.isAttacking;
+        this.health = 100;
+        this.dead = false;
+        this.image = new Image();
+        this.image.src = imageSrc;
+        this.framesMax = framesMax;
+        this.framesElapsed = 0;
+        this.framesHold = 5;
+        this.enemyDeadFrameConfirmed=false;
+        this.framesCurrent = 0;
+        this.scale = scale;
+        this.offset = offset;
+        this.sprites = sprites;
+        this.type=type;
+        this.off=false;
+
+        for (const sprite in this.sprites) {
+            sprites[sprite].image = new Image();
+            sprites[sprite].image.src = sprites[sprite].imgSrc;
+        }
+
+        console.log(this.sprites);
+    }
+    
+
+    drawPlayer() {
+        
+            c.fillStyle = 'blue';
+            c.font = `30px Verdana`;
+            c.fillText("Player 1", this.position.x - 40, this.position.y + 20, 70);
+
+            c.drawImage(this.image, this.framesCurrent * (this.image.width / this.framesMax), 0,
+                this.image.width / this.framesMax, this.image.height, this.position.x - this.offset.x, this.position.y - this.offset.y,
+                (this.image.width / this.framesMax) * this.scale, this.image.height * this.scale);
+
+        
+        if (this.isAttacking) {
+            c.fillStyle = 'blue';
+
+
+            c.fillText("Attack!", this.hitBox.position.x + 20, this.hitBox.position.y - 50, this.hitBox.width, this.hitBox.height);
+            c.fillStyle = 'rgba(225,225,225,0)';
+            let attackRect = c.fillRect(this.hitBox.position.x + 30, this.hitBox.position.y, this.hitBox.width, this.hitBox.height);
+
+        }
+    }
+
+    drawEnemy() {
+       
+            c.fillStyle = 'red';
+            c.font = `30px Verdana`;
+            c.fillText("Player 2", this.position.x - 40, this.position.y + 20, 70);
+            c.drawImage(this.image, this.framesCurrent * (this.image.width / this.framesMax), 0,
+                this.image.width / this.framesMax, this.image.height, this.position.x - this.offset.x, this.position.y - this.offset.y,
+                (this.image.width / this.framesMax) * this.scale, this.image.height * this.scale);
+            if (this.isAttacking) {
+                c.fillStyle = 'red';
+                c.fillText("Attack!", this.hitBox.position.x - 35, this.hitBox.position.y - 50, this.hitBox.width, this.hitBox.height);
+                c.fillStyle = 'rgba(225,225,225,0)';
+                let attackRectEnemy = c.fillRect(this.hitBox.position.x + 30, this.hitBox.position.y, this.hitBox.width, this.hitBox.height);
+
+            }
+    }
+
+
+
+    update(gravity) {
+        if (this.dead) {
+            this.velocity.x = 0;
+            this.velocity.y = 0;
+            return; // Stop further updates if the player is dead
+        }
+    
+        this.framesElapsed++;
+    
+        if (this.image == this.sprites.death.image && this.type === 'enemy') {
+            this.keep = true;
+            console.log('should be ded');
+            if (this.framesCurrent == 0 && this.framesCurrent != 6) {
+                this.framesCurrent = 6;
+            }
+            if (this.framesElapsed % this.framesHold == 0) {
+                if (this.framesCurrent >= 0) {
+                    this.framesCurrent--;
+                }
+                if (this.framesCurrent <= 1) {
+                    this.enemyDeadFrameConfirmed = true;
+                    
+                    this.dead=true;
+                }
+            }
+        } else if (this.framesElapsed % this.framesHold === 0) {
+            if (this.framesCurrent < this.framesMax - 1) {
+                this.framesCurrent++;
+            } else {
+                this.framesCurrent = 0;
+                
+            }
+        }
+    
+        // Apply movement if not dead
+        this.position.x += this.velocity.x;
+        this.position.y += this.velocity.y;
+    
+        // Update hitbox
+        this.hitBox.position.x = this.position.x - 110;
+        this.hitBox.position.y = this.position.y;
+    
+        // Gravity
+        if (this.position.y + this.height + this.velocity.y >= canvas.height) {
+            this.position.y = 425.76;
+            this.jumpCounter = 0;
+        } else {
+            this.jumpCounter = 1;
+            this.velocity.y += gravity;
+        }
+    }
+    
+
+    getJumpCounter() {
+        console.log(this.jumpCounter);
+        return this.jumpCounter;
+    }
+
+    attack(index,lastKey) {
+        if(this.type=='player'){
+        if(lastKey=='d'){
+        if(index==1){
+        this.switchSprite('attack1');
+        }
+        else{
+         this.switchSprite('attack2');
+        }
+        this.isAttacking = true;
+        setTimeout(() => {
+            this.switchSprite('idle');
+            this.isAttacking = false;
+        }, 250);
+    }
+    else if(lastKey=='a'){
+        if(index==1 ){
+            this.switchSprite('attack1Opposite');
+            }
+            else{
+             this.switchSprite('attack2Opposite');
+            }
+            this.isAttacking = true;
+            setTimeout(() => {
+                this.switchSprite('idleOpposite');
+                this.isAttacking = false;
+            }, 250);
+    }
+    else{
+        if(index==1){
+            this.switchSprite('attack1');
+            }
+            else{
+             this.switchSprite('attack2');
+            }
+            this.isAttacking = true;
+            setTimeout(() => {
+                this.switchSprite('idle');
+                this.isAttacking = false;
+            }, 250);
+    }
+    }
+    else if(this.type==='enemy'){
+        if(lastKey=='ArrowLeft'){
+            if(index==1){
+            this.switchSprite('attack1');
+            }
+            else{
+             this.switchSprite('attack2');
+            }
+            this.isAttacking = true;
+            setTimeout(() => {
+                this.switchSprite('idle');
+                this.isAttacking = false;
+            }, 250);
+        }
+        else if(lastKey=='ArrowRight'){
+            if(index==1 ){
+                this.switchSprite('attack1Opposite');
+                }
+                else{
+                 this.switchSprite('attack2Opposite');
+                }
+                this.isAttacking = true;
+                setTimeout(() => {
+                    this.switchSprite('idleOpposite');
+                    this.isAttacking = false;
+                }, 250);
+        }
+        else{
+            if(index==1){
+                this.switchSprite('attack1');
+                }
+                else{
+                 this.switchSprite('attack2');
+                }
+                this.isAttacking = true;
+                setTimeout(() => {
+                    this.switchSprite('idle');
+                    this.isAttacking = false;
+                }, 250);
+        }
+    }
+    }
+
+
+    switchSprite(sprite) {
+
+        //OVERRIDING ALL OTHER ANIMATIONS WITH THE ATTACK ANIMATION
+        if(this.image == this.sprites.attack1.image && this.framesCurrent < this.sprites.attack1.framesMax-1){ return }
+       
+        //ALSO WITH ATTACK ANIMATION 2
+        if(this.image == this.sprites.attack2.image && this.framesCurrent < this.sprites.attack2.framesMax-1){ return }
+
+        //OPPOSITE ATTACKS 1 AND 2
+
+        if(this.image == this.sprites.attack1Opposite.image && this.framesCurrent < this.sprites.attack1Opposite.framesMax-1){ return }
+       
+       
+        if(this.image == this.sprites.attack2Opposite.image && this.framesCurrent < this.sprites.attack2Opposite.framesMax-1){ return }
+
+        //OVERRIDE WHEN FIGHTER GETS HIT
+        if(this.image == this.sprites.takeHit.image && this.framesCurrent < this.sprites.takeHit.framesMax-1){ return }
+       
+       //OVERRIDE WHEN FIGHTER DIES
+
+       if(this.image == this.sprites.death.image && this.framesCurrent < this.sprites.death.framesMax-1){ return }
+       
+       if(this.image == this.sprites.jump.image && this.framesCurrent < this.sprites.jump.framesMax-1){ return }
+       //enemy
+        if(this.image == this.sprites.death.image && this.type=='enemy') { return };
+
+        switch (sprite) {
+            case 'idle': if (this.image != this.sprites.idle.image) {
+                this.image = this.sprites.idle.image; this.framesMax = this.sprites.idle.framesMax; this.framesCurrent=0;
+            } break;
+            case 'run': if (this.image != this.sprites.run.image ) {
+                this.image = this.sprites.run.image; this.framesMax = this.sprites.run.framesMax; this.framesCurrent=0;
+            } break;
+            case 'jump': if (this.image != this.sprites.jump.image || this.image!= this.sprites.run.image && this.position.y + this.height + this.velocity.y <= canvas.height) {
+                this.image = this.sprites.jump.image;
+                this.framesMax = this.sprites.jump.framesMax;
+                this.framesCurrent=0;
+            } break;
+            case 'attack1':  if (this.image != this.sprites.attack1.image){
+                this.image = this.sprites.attack1.image;
+                this.framesMax = this.sprites.attack1.framesMax;
+                this.framesCurrent=0;
+            }break;
+            case 'attack2':  if (this.image != this.sprites.attack2.image){
+                this.image = this.sprites.attack2.image;
+                this.framesMax = this.sprites.attack2.framesMax;
+                this.framesCurrent=0;
+            }break;
+            case 'takeHit' : if(this.image != this.sprites.takeHit.image){
+                this.image = this.sprites.takeHit.image;
+                this.framesMax = this.sprites.takeHit.framesMax;
+                this.framesCurrent=0;
+            }break;
+            case 'death' : if(this.image != this.sprites.death.image){
+                this.image = this.sprites.death.image;
+                this.framesMax = this.sprites.death.framesMax;
+                this.framesCurrent=0;
+            }break;
+            case 'fall': if(this.image != this.sprites.fall.image){
+                this.image = this.sprites.fall.image;
+                this.framesMax = this.sprites.fall.framesMax;
+                this.framesCurrent=0;
+            }break;
+
+            case 'idleOpposite': if (this.image != this.sprites.idleOpposite.image) {
+                this.image = this.sprites.idleOpposite.image; this.framesMax = this.sprites.idleOpposite.framesMax; this.framesCurrent=0;
+            } break;
+            case 'runOpposite': if (this.image != this.sprites.runOpposite.image ) {
+                this.image = this.sprites.runOpposite.image; this.framesMax = this.sprites.runOpposite.framesMax; this.framesCurrent=0;
+            } break;
+            case 'jumpOpposite': if (this.image != this.sprites.jumpOpposite.image || this.image!= this.sprites.runOpposite.image && this.position.y + this.height + this.velocity.y <= canvas.height) {
+                this.image = this.sprites.jumpOpposite.image;
+                this.framesMax = this.sprites.jumpOpposite.framesMax;
+                this.framesCurrent=0;
+            } break;
+            case 'attack1Opposite':  if (this.image != this.sprites.attack1Opposite.image){
+                this.image = this.sprites.attack1Opposite.image;
+                this.framesMax = this.sprites.attack1Opposite.framesMax;
+                this.framesCurrent=0;
+            }break;
+            case 'attack2Opposite':  if (this.image != this.sprites.attack2Opposite.image){
+                this.image = this.sprites.attack2Opposite.image;
+                this.framesMax = this.sprites.attack2Opposite.framesMax;
+                this.framesCurrent=0;
+            }break;
+            case 'takeHitOpposite' : if(this.image != this.sprites.takeHitOpposite.image){
+                this.image = this.sprites.takeHitOpposite.image;
+                this.framesMax = this.sprites.takeHitOpposite.framesMax;
+                this.framesCurrent=0;
+            }break;
+            case 'fallOpposite': if(this.image != this.sprites.fallOpposite.image){
+                this.image = this.sprites.fallOpposite.image;
+                this.framesMax = this.sprites.fallOpposite.framesMax;
+                this.framesCurrent=0;
+            }break;
+        }
+    }
+
+    takeHit(){
+        
+        this.health-=10;
+       console.log(this.type+" health: " +this.health);
+        if(this.health<=0){
+            console.log("sprite switched");
+            console.log('dead?');
+            this.switchSprite('death');
+        }else{
+            this.switchSprite('takeHit');
+            
+        }
+        
+    }
+    
+}
+
+
+
+
+
+export { Sprite };
---
+++ MyFightingGame/infrastructure/index.js
@@ -0,0 +1,452 @@
+import { SpriteFunctions } from '../application/SpriteFunctions.js';
+import { BackgroundSprite } from '../domain/BackgroundSprite.js';
+import { GameService } from '../domain/Services/GameService.js';
+
+const canvas = document.querySelector('canvas');
+const c = canvas.getContext('2d');
+
+const SERVICE = new GameService();
+
+canvas.width = 1024;
+canvas.height = 576;
+
+c.fillRect(0, 0, canvas.width, canvas.height);
+
+const sprites = new SpriteFunctions();
+
+const gravity = 0.24;
+
+//TIMER VALUE IN SECONDS
+let timer = 60;
+
+let timerElement = document.createElement("p");
+timerElement.style.fontSize = "20px"; // Fix fontSize typo from "20x" to "20px"
+timerElement.style.margin = "25px";
+
+let timerBox = document.querySelector('.timer');
+timerBox.appendChild(timerElement);
+
+let enemyHealthBar = document.querySelector('#enemyHealth');
+let playerHealthBar = document.querySelector('#playerHealth');
+
+let healthBars = document.querySelector('.healthsContainer');
+let container = document.querySelector('.gameOverContainer');
+
+// Create player and enemy objects
+sprites.loadPlayer(80, 150);
+sprites.loadEnemy(925, 150);
+
+let jumpCounter = 0;
+
+let elementGameOver = document.createElement('h1');
+
+// Background Image
+const background = new BackgroundSprite({
+    position: { x: 0, y: 0 },
+    imageSrc: '../assets/oak_woods_v1.0/background/background_layer_1.png'
+});
+
+
+
+
+let audioPlayed = false;
+
+//AUDIO FROM ATTACKS
+
+let getHitSE = document.querySelector('.getHit');
+
+let jumpSE = document.querySelector('.jump');
+
+
+document.addEventListener('DOMContentLoaded', () => {
+    // Handle audio start event outside the animation loop
+    document.addEventListener('click', () => {
+
+        if (!audioPlayed) {
+            let audio = document.body.querySelector('.bgm');
+            audio.load();
+            audio.play();
+            audio.loop = true;
+            audioPlayed = true;
+        }
+
+    });
+});
+
+// Animation Loop
+let isKeyADown = false; // Tracks if 'a' is pressed
+let isKeyDDown = false; // Tracks if 'd' is pressed
+
+let keyArrowDownLeft = false;
+let keyArrowDownRight = false;
+
+let lastKey;
+
+let lastKeyEnemy;
+
+// Movement Events for Sprites
+window.addEventListener('keydown', (event) => {
+    switch (event.key) {
+        case 'a':
+            isKeyADown = true;
+            sprites.player.velocity.x = -5;
+            lastKey = 'a';
+            break;
+        case 'd':
+            isKeyDDown = true;
+            sprites.player.velocity.x = 5;
+            lastKey = 'd';
+            break;
+        case ' ':
+            if (sprites.player.getJumpCounter() == 0) {
+
+                if (lastKey === 'd') {
+                    sprites.player.velocity.y = -8;
+                    sprites.player.switchSprite('jump');
+                    jumpSE.load();
+                    jumpSE.play();
+                }
+                if (lastKey === 'a') {
+                    sprites.player.velocity.y = -8;
+                    sprites.player.switchSprite('jumpOpposite');
+                    jumpSE.load();
+                    jumpSE.play();
+                }
+
+            }
+            break;
+
+
+        case 'ArrowLeft':
+            keyArrowDownLeft = true;
+            sprites.enemy.velocity.x = -5;
+            lastKeyEnemy = 'ArrowLeft';
+
+            break;
+        case 'ArrowRight':
+            keyArrowDownRight = true;
+            sprites.enemy.velocity.x = 5;
+            lastKeyEnemy = 'ArrowRight';
+
+            break;
+        case '0': // Enemy jump
+            if (sprites.enemy.getJumpCounter() == 0) {
+
+                if (lastKeyEnemy === 'ArrowLeft') {
+                    sprites.enemy.velocity.y = -8;
+                    sprites.enemy.switchSprite('jump');
+                    jumpSE.load();
+                    jumpSE.play();
+                }
+                if (lastKeyEnemy === 'ArrowRight') {
+                    sprites.enemy.velocity.y = -8;
+                    sprites.enemy.switchSprite('jumpOpposite');
+                    jumpSE.load();
+                    jumpSE.play();
+                }
+
+            }
+            break;
+
+    }
+});
+
+window.addEventListener('keyup', (event) => {
+    switch (event.key) {
+
+        case 'a':
+            isKeyADown = false;
+            if (!isKeyDDown) sprites.player.velocity.x = 0;
+            break; // Fix missing break
+
+        case 'd':
+            isKeyDDown = false;
+            if (!isKeyADown) sprites.player.velocity.x = 0;
+            break; // Fix missing break
+
+        case ' ':
+            if (sprites.player.getJumpCounter() == 0) {
+                sprites.player.velocity.y = 0;
+            }
+            break;
+
+        case 'ArrowLeft':
+            keyArrowDownLeft = false;
+            if (!keyArrowDownRight) {
+                sprites.enemy.velocity.x = 0;
+
+            }
+            break;
+        case 'ArrowRight':
+            keyArrowDownRight = false;
+            if (!keyArrowDownLeft) {
+                sprites.enemy.velocity.x = 0;
+
+            }
+            break;
+        case '0': // Reset jump animation
+            if (sprites.enemy.getJumpCounter() === 0) {
+                sprites.enemy.velocity.y = 0;
+            }
+            break;
+    }
+});
+
+let playGetHitSe =  getHitSE.play();
+
+// Animation Loop
+function animateSprites() {
+    window.requestAnimationFrame(animateSprites);
+
+    if (timer <= 10 && !sprites.player.dead && !sprites.enemy.dead) {
+        timerElement.style.margin = "35px";
+    }
+
+    // Switch to idle animation if no horizontal movement keys are pressed
+    if (sprites.player.position.y + sprites.player.height + sprites.player.velocity.y >= canvas.height) {
+        if (!isKeyADown && lastKey === 'a') {
+            sprites.player.switchSprite('idleOpposite');
+        } else if (!isKeyDDown && lastKey === 'd') {
+            sprites.player.switchSprite('idle');
+        } else if (isKeyDDown) {
+            sprites.player.switchSprite('run');
+
+        } else if (isKeyADown) {
+            sprites.player.switchSprite('runOpposite');
+        }
+
+
+    }
+    if (sprites.enemy.position.y + sprites.enemy.height + sprites.enemy.velocity.y >= canvas.height) {
+        if (!keyArrowDownLeft && lastKeyEnemy === 'ArrowLeft') {
+            sprites.enemy.switchSprite('idle');
+        } else if (!keyArrowDownRight && lastKeyEnemy === 'ArrowRight') {
+            sprites.enemy.switchSprite('idleOpposite');
+
+        } else if (keyArrowDownLeft) {
+            sprites.enemy.switchSprite('run');
+        } else if (keyArrowDownRight) {
+            sprites.enemy.switchSprite('runOpposite');
+        }
+    }
+
+    // Update the timer countdown
+
+    SERVICE.updateTimer(timer);
+    
+
+
+    if (timer <= 0 && timer > -1 || sprites.enemy.dead || sprites.player.dead) {
+        timer = -1;
+
+        elementGameOver.style.zIndex = '999';
+        elementGameOver.style.alignSelf = 'center';
+        elementGameOver.style.position = 'absolute';
+        elementGameOver.style.marginLeft = '450px';
+        elementGameOver.style.marginTop = '450px';
+        container.appendChild(elementGameOver);
+        if (sprites.enemy.dead) {
+
+            elementGameOver.textContent = 'PLAYER 1 WINS';
+
+            cancelAnimationFrame();
+
+
+        }
+        if (sprites.player.dead) {
+
+            elementGameOver.textContent = 'PLAYER 2 WINS';
+            cancelAnimationFrame();
+
+
+
+        }
+    }
+
+   
+    
+
+    //TRIGGER FALL ANIMATIONS
+
+    if (sprites.player.getJumpCounter() == 1 && sprites.player.position.y < 300) {
+        if (lastKey === 'd') {
+            sprites.player.switchSprite('fall');
+        }
+        else if (lastKey === 'a') {
+            sprites.player.switchSprite('fallOpposite');
+        }
+    }
+
+
+    if (sprites.enemy.getJumpCounter() == 1 && sprites.enemy.position.y < 300) {
+        if (lastKeyEnemy === 'ArrowLeft') {
+            sprites.enemy.switchSprite('fall');
+        }
+        else if (lastKeyEnemy === 'ArrowRight') {
+            sprites.enemy.switchSprite('fallOpposite');
+        }
+    }
+
+        // End of game cleanup
+        if (timer == -1) {
+            /*document.querySelector('.timer').remove();
+            healthBars.remove();*/
+            timer = 0;
+            timerElement.textContent = timer;
+            elementGameOver.textContent = 'GAME OVER';
+            cancelAnimationFrame();
+
+        }
+
+
+
+        c.fillStyle = 'black';
+        c.fillRect(0, 0, canvas.width, canvas.height);
+        timerElement.textContent = parseInt(timer);
+
+        // Update background
+        background.update();
+
+        // Update player and enemy
+        sprites.player.update(gravity, jumpCounter);
+        sprites.enemy.update(gravity, jumpCounter);
+
+        // Draw player and enemy
+        sprites.player.drawPlayer();
+        sprites.enemy.drawEnemy();
+
+        
+
+        // Collision detection and enemy Hit
+        if (SERVICE.hasCollided(sprites.player,sprites.enemy)) {
+            
+            getHitSE.load();
+            getHitSE.play();
+            SERVICE.decreaseHealth(sprites.player,sprites.enemy,enemyHealthBar,playerHealthBar);
+        }
+    
+
+        // Enemy death event
+        if (sprites.enemy.health <= 0 && sprites.enemy.enemyDeadFrameConfirmed) {
+
+            sprites.enemy.dead = true;
+
+        }
+
+        // Player death event
+        if (sprites.player.health <= 0) {
+            if (sprites.player.framesCurrent == sprites.player.sprites.death.framesMax - 1) {
+                sprites.player.dead = true;
+            }
+
+        }
+
+    }
+
+    animateSprites();
+
+
+
+
+    // Declare audio elements for player and enemy attack sounds
+    let audioAtkPlayer = document.querySelector('#atkPlayer');
+    let audioAtkEnemy = document.querySelector('#atkEnemy');
+
+    // Player attack key 'e' press
+    // Declare audio elements for player and enemy attack sounds
+
+    let canAttackPlayer = true; // Attack cooldown flag
+    const attackCooldownPlayer = 300; // Cooldown in milliseconds (adjust as needed)
+
+    // Player attack key 'e' press or 'q' press for attack2
+    window.addEventListener('keydown', (event) => {
+        if (event.key === 'e' && canAttackPlayer) {
+
+            canAttackPlayer = false; // Prevent further attacks until cooldown ends
+
+            sprites.player.attack(1, lastKey);
+
+            // Play attack sound
+            if (audioAtkPlayer) {
+                if (!audioAtkPlayer.paused) {
+                    audioAtkPlayer.currentTime = 0;
+                }
+                audioAtkPlayer.play();
+            }
+
+            // Re-enable attack after cooldown
+            setTimeout(() => {
+                canAttackPlayer = true;
+            }, attackCooldownPlayer);
+
+
+        }
+
+        if (event.key === 'q' && canAttackPlayer) {
+            canAttackPlayer = false; // Prevent further attacks until cooldown ends
+
+            sprites.player.attack(2, lastKey);
+
+            // Play attack sound
+            if (audioAtkPlayer) {
+                if (!audioAtkPlayer.paused) {
+                    audioAtkPlayer.currentTime = 0;
+                }
+                audioAtkPlayer.play();
+            }
+
+            // Re-enable attack after cooldown
+            setTimeout(() => {
+                canAttackPlayer = true;
+            }, attackCooldownPlayer);
+        }
+    });
+
+
+    let canAttackEnemy = true; // Attack cooldown flag
+    const attackCooldownEnemy = 300; // Cooldown in milliseconds
+
+    // Enemy attack key 'Enter' press
+    window.addEventListener('keydown', (event) => {
+        if (event.key === 'Enter' && canAttackEnemy) {
+            canAttackEnemy = false;
+
+            // Trigger enemy attack
+            sprites.enemy.attack(1, lastKeyEnemy);
+
+            // Play attack sound if available
+            if (audioAtkEnemy) {
+                if (!audioAtkEnemy.paused) {
+                    audioAtkEnemy.currentTime = 0; // Reset audio to start
+                }
+                audioAtkEnemy.play();
+            }
+
+            // Cooldown reset
+            setTimeout(() => {
+                canAttackEnemy = true;
+            }, attackCooldownEnemy);
+        }
+
+        if (event.key === 'Backspace' && canAttackEnemy) {
+            canAttackEnemy = false; // Prevent further attacks until cooldown ends
+
+            sprites.enemy.attack(2, lastKeyEnemy);
+
+            // Play attack sound
+            if (audioAtkEnemy) {
+                if (!audioAtkEnemy.paused) {
+                    audioAtkEnemy.currentTime = 0; // Reset audio to start
+                }
+                audioAtkEnemy.play();
+            }
+
+            // Cooldown reset
+            setTimeout(() => {
+                canAttackEnemy = true;
+            }, attackCooldownEnemy);
+        }
+    });
+
+    console.log(sprites.player);
+    console.log(sprites.enemy);
